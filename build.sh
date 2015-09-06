60c39f53e0fbc9478b03716740b506d2935f3a4e
Gabriel Engel <gabriel.engel@fgsys.com>
2015-09-06 20:47:45 -0300

    fix directory

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ if [ $1 ]; then
 	source ./build-info.sh
 	export METEOR_SETTINGS=$(cat settings.$1.json)
 	meteor build --server http://chat.woojou.com:3000 --directory /home/woojou/public_html/chat_bundle
-	cd /home/woojou/public_html/chat_bundle/programs/server
+	cd /home/woojou/public_html/chat_bundle/bundle/programs/server
 	npm install
 	cd /home/woojou/public_html/chat
 	pm2 startOrRestart /home/woojou/public_html/chat/pm2.$1.json
